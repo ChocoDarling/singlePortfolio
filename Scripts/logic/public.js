092949604ece0ef1dad838f81b5aa10f39181168
ChocoDarling <rudekrudek@naver.com>
2020-09-09 11:39:48 +0900

    big_cast_complete

--- Scripts/logic/public.js
+++ Scripts/logic/public.js
@@ -1,6 +1,12 @@
-class MakeHTMLElement extends HTMLElement {
-  constructor() {
-      super();
-  }
-  
+
+function makeElement(attributesArray = [], parentId = '', tag = 'div', isAppend = true) {
+  let parentElement = document.body;
+  if (parentId) parentElement = document.getElementById(parentId);
+  let tempElement = document.createElement(tag);
+  if (isAppend) parentElement.append(tempElement);
+  else parentElement.prepend(tempElement);
+  attributesArray.forEach(attributes => {
+      if (attributes.length === 3) tempElement[attributes[0]][attributes[1]] = attributes[2];
+      else tempElement[attributes[0]] = attributes[1];
+  });
 }
