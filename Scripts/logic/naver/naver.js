f9a3ec5bf5facc8f14e2cda93b30d5295923bf41
ChocoDarling <rudekrudek@naver.com>
2020-09-11 17:41:22 +0900

    20200911-17:41_naver_giveup

--- Scripts/logic/naver/naver.js
+++ Scripts/logic/naver/naver.js
@@ -1,9 +1,14 @@
 function menuPlus() {
   if(document.getElementById('topMenuPlusDiv').style.display === 'none') {
+    document.getElementById('topMenuPlus').innerHTML = '접기';
+    document.getElementById('topMenuPlus').style.color = 'rgb(0, 200, 80)';
     document.getElementById('topDiv').style.boxShadow = '';
     document.getElementById('topMenuPlusDiv').style.display = 'block';
   } else {
+    document.getElementById('topMenuPlus').innerHTML = '더보기';
+    document.getElementById('topMenuPlus').style.color = 'black';
     document.getElementById('topMenuPlusDiv').style.display = 'none';
     document.getElementById('topDiv').style.boxShadow = '0px 10px 10px 0px lightgray';
   }
 }
+
